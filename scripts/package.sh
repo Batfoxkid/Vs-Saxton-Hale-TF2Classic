c74331a6c2ffbc53dab10fa9bf088bfd31ae3b74
Batfoxkid <batfoxkid@gmail.com>
2020-12-10 12:48:25 -0800

    Add cfg to Package

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,10 +2,12 @@
 cd build
 
 # Create package dir
+mkdir -p package/cfg
 mkdir -p package/addons/sourcemod/plugins
 
 # Copy all required stuffs to package
 cp -r addons/sourcemod/plugins/cvsh.smx package/addons/sourcemod/plugins
+cp -r cfg/randomizer_vsh.cfg package/cfg
 cp -r ../models package
 cp -r ../materials package
 cp -r ../sound package
